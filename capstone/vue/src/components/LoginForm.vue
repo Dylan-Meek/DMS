291a26b0bb33bb7ef074e40fa9f9eb150ff11a33
Dylan Meek <meekdm@gmail.com>
2022-08-08 15:43:06 -0400

    Login/Registration Page

---
+++ capstone/vue/src/components/LoginForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style>
+form {
+  padding-top: 2%;
+  padding-bottom: 1%;
+  font-size: 16px;
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 35%;
+  margin-right: 35%;
+  margin-top: 5%;
+  background-color: #162228;
+  color: #f9f7f7;
+  border-radius: 40px;
+}
+
+form > input {
+  margin-bottom: 20px;
+  margin-left: 20%;
+  margin-right: 20%;
+}
+
+label {
+  padding-bottom: 10px;
+  margin-left: 20%;
+  margin-right: 20%;
+  color: #f9f7f7;
+}
+
+h1 {
+  margin-left: auto;
+  margin-right: auto;
+  color: #f9f7f7;
+}
+
+.link {
+  padding: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  color: #f9f7f7;
+  font-size: 0.7rem;
+}
+
+button {
+  margin-left: auto;
+  margin-right: auto;
+  color: #162228;
+  background-color: #ffde32;
+  border-radius: 5px;
+  width: 25%;
+  align-content: center;
+  font-size: 0.9rem;
+}
+
+body {
+  background-image: url("/62f153a316532.jpg");
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+  background-position: center;
+}
+</style>
