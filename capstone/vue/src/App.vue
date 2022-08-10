5fc92780cf2fa7f0ffdde68c4324b015832bda07
Jeremy Falk <falk.jeremy.r@gmail.com>
2022-08-10 11:14:53 -0400

    styled logout button

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -7,16 +7,24 @@
       <router-link
         v-bind:to="{ name: 'logout' }"
         v-if="$store.state.token != ''"
-        >Logout</router-link
-      >
+        ><button type="submit">Logout</button>
+      </router-link>
     </div>
     <router-view />
   </div>
 </template>
-
 <style scoped>
 img {
   width: 15%;
   height: 10%;
+}
+button {
+  margin-right: 100%;
+  color: #f9f7f7;
+  background-color: #162228;
+  border-radius: 5px;
+  width: 10%;
+  align-content: center;
+  font-size: 0.9rem;
 }
 </style>
