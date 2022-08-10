351ef342a46c61f7ebd0dadde51c390a834bfc3f
Mark Baron <mark_baron@outlook.com>
2022-08-10 11:30:35 -0400

    car card close to finish

--- capstone/vue/src/components/CarCard.vue
+++ capstone/vue/src/components/CarCard.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="card">
-    <img v-bind:src="`${car.photo}`" />
+    <img id="car-image" v-bind:src="`${car.photo}`" />
     <div class="flex-car">
-      <ul>
-        <li>{{ car.year + " " + car.make + " " + car.model }}</li>
-        <li>{{ car.mileage + " Miles | " + car.engine + " Engine" }}</li>
-        <li>{{ "$" + car.price + " | " + determineStatus() }}</li>
-        <li>{{ car.notes }}</li>
-      </ul>
+      <h3>{{ car.year + " " + car.make + " " + car.model }}</h3>
+      <h4>{{ "$" + car.price }}</h4>
+      <h4>{{ car.mileage + " Miles" }}</h4>
+      <button>Purchase</button>
     </div>
   </div>
 </template>
@@ -32,32 +30,45 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital@1&display=swap");
+
 .card {
   display: flex;
-  border: 4px solid black;
+  border: 2px solid black;
   border-radius: 10px;
-  width: 900px;
-  height: 550px;
-  margin: 20px;
+  width: 20vw;
+  height: 60vh;
+  min-width: 275px;
+  min-height: 400px;
   background-color: rgba(255, 255, 255, 0.7);
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+  flex-direction: column;
+  font-family: "Montserrat", "Helvetica";
 }
 
-img {
-  width: 45%;
-  height: 90%;
+#car-image {
+  width: 85%;
+  height: 50%;
+  padding: 10px;
+  object-fit: cover;
 }
 
 .flex-car {
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 0;
 }
 
-ul {
-  font-size: 30px;
-}
-
-ul > li {
-  padding-bottom: 50px;
+button {
+  margin-left: auto;
+  margin-right: auto;
+  color: #162228;
+  background-color: #ffde32;
+  border-radius: 5px;
+  width: 25%;
+  align-content: center;
+  font-size: 0.9rem;
 }
 </style>
