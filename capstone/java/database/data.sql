724f642268f0782a52c315ad10dfe2911b60383c
Zack Raney <z.raney@gmail.com>
2022-08-18 14:55:45 -0400

    submitting database changes

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -22,10 +22,7 @@ INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo
 VALUES ('1lk32451k4b', 'Nissan', 'Cursebox', 1969, 420000, 'V1', 2, 4200, 'https://i.insider.com/5637e2ba9dd7ccfc418bb91e?width=1300&format=jpeg&auto=webp', 'https://www.netcarshow.com/Dodge-Caravan-1984-1280-05.jpg', 'https://www.netcarshow.com/Dodge-Caravan-1984-1280-04.jpg','', false);
 
 INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
-VALUES ('l1kj3b2l4hkr5b', 'Honda', 'Civic', 2000, 169000, 'V3', 2, 3500, 'https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2Fyc3xlbnwwfHwwfHw%3D&w=1000&q=80','','','', true);
-
-INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
-VALUES ('li1jb4i2tyb3rf', 'Chevy', 'Truck', 2005, 11, 'V8', 2, 6900, 'https://i.cbc.ca/1.5315974.1570713153!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/ransford-lockhart-truck-explosion.jpg', '','','', true);
+VALUES ('firetruck', 'Chevy', 'Truck', 2005, 11, 'V8', 2, 6900, 'https://i.cbc.ca/1.5315974.1570713153!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/ransford-lockhart-truck-explosion.jpg', '','','', false);
 
 INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
 VALUES ('abcjb4i2tyb123', 'Ferrari', 'F430', 2003, 6000, 'V10', 2, 95000, 'https://www.ryanfriedmanmotorcars.com/imagetag/1290/23/l/Used-2003-Ferrari-360-Spider-1650764253.jpg', 'https://www.ryanfriedmanmotorcars.com/imagetag/1290/18/l/Used-2003-Ferrari-360-Spider-1650764253.jpg','https://www.ryanfriedmanmotorcars.com/imagetag/936/15/l/Used-2003-Ferrari-360-Spider-1615216607.jpg','https://www.ryanfriedmanmotorcars.com/imagetag/936/8/f/Used-2003-Ferrari-360-Spider-1615216607.jpg', true);
@@ -34,26 +31,24 @@ INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo
 VALUES ('li1jb4i2tyb444', 'Ferrari', 'F430', 2012, 5000, 'V12', 2, 120000, 'https://get.wallhere.com/photo/car-vehicle-sports-car-Ferrari-Ferrari-F430-performance-car-2012-netcarshow-netcar-car-images-car-photo-F430-by-Status-Design-supercar-land-vehicle-automotive-design-automobile-make-luxury-vehicle-ferrari-spa-ferrari-f430-challenge-ferrari-360-391666.jpg', '','','', true);
 
 INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
-VALUES ('li1jb4i2tyb555', 'Kia', 'Carnival', 2022, 500, 'V6', 2, 44000, 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/20201013_Kia_Carnival_Front_Side_%28cropped%29.jpg/1920px-20201013_Kia_Carnival_Front_Side_%28cropped%29.jpg', '','','', true);
-
-INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
-VALUES ('li1jb4i2tyb666', 'GMC', '', 1983, 120000, 'V8', 2, 26000, 'https://12pointsignworks.com/wp-content/uploads/2018/10/A-Team-Van-Mr-T.jpg','','','', true);
-
-INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
-VALUES ('abcjb4i2tyb777', 'Chevy', 'Corvette', 2022, 815, 'V10', 2, 66000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoyHqnJlq_KUFKUIUMxz3k9YHLY9XHAR-cfkg74lG0iSUQ3JgMJvMIPa607xYETwBdNBw&usqp=CAU','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_lzMCzYN_clxBhkUubAwA5BjYT9tLCkFF1WCOBNVXSbdv0xiSOAj3mxu6gdOA4J3Jm9Q&usqp=CAU','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmZZV80b89qFiI0IixDxtoJM1QjcS_ysVAmOzOkXxh4UAdByydStzosU3ECn7-JWE5VkQ&usqp=CAU','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGcLelRQ_PS3Ofx6bjHLLfc8v4IJPJeVN0xx2IfvgPKrwlHzlKzeWFnXpfn7HnywqvmMI&usqp=CAU', true);
-
-Insert into garage (vin, user_id)
-VALUES ('1lk32451k4b', 3);
-
-INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
 VALUES ('li1jb4i2tyb777', 'Mercedes', 'GT R', 2021, 3000, 'V8', 2, 88000, 'https://cdn.carbuzz.com/gallery-images/1600/589000/400/589431.jpg', '','','', true);
 
 INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
 VALUES ('li1jb4i2tyb888', 'Mercedes', 'C63', 2010, 4000, 'V8', 2,60000, 'https://autotraderau-res.cloudinary.com/image/upload/t_gl/v1/glasses/DBTMKV0S.jpg', '','','', true);
 
 INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
-VALUES ('li1jb4i2tyb999', 'Chevrolet', 'Impala', 2018, 30000, 'V6', 2, 26000, 'https://di-uploads-development.dealerinspire.com/betleychevy-legacymigration0720/uploads/2020/07/2018ChevyImpala-1-1024x595.jpg', '','','', true);
+VALUES ('vin1zr', 'Jeep', 'Wrangler', 2021, 6000, '6-cyl', 3, 48900, 'https://img2.carmax.com/img/vehicles/22843221/4.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/22843221/3.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/22843221/6.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/22843221/10.jpg?width=1600&ratio=source', true);
 
+INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
+VALUES (vin2zr, 'Jeep', 'Compass', 2022, 6, '4-cyl', 1, 38000,'https://img2.carmax.com/img/vehicles/23100082/1.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/23100082/3.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/23100082/6.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/23100082/10.jpg?width=1600&ratio=source', true);
 
+INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
+VALUES (vin3zr, 'Jeep', 'Cherokee', 2022, 20, '6-cyl', 1, 45000, 'https://img2.carmax.com/img/vehicles/22843653/1.jpg?width=800&ratio=4/3', 'https://img2.carmax.com/img/vehicles/22843653/3.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/22843653/6.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/22843653/10.jpg?width=1600&ratio=source', true);
+
+INSERT INTO car (vin, make, model, year, mileage, engine, input_id, price, photo, photo_2, photo_3, photo_4, is_for_sale)
+VALUES (vin4zr, 'Jeep', 'Gladiator', 2021, 63000, '6-cyl', 3, 52,998, 'https://img2.carmax.com/img/vehicles/22957490/1.jpg?width=800&ratio=4/3', 'https://img2.carmax.com/img/vehicles/22957490/3.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/22957490/6.jpg?width=1600&ratio=16/9', 'https://img2.carmax.com/img/vehicles/22957490/10.jpg?width=1600&ratio=source', true);
+
+Insert into garage (vin, user_id)
+VALUES ('1lk32451k4b', 3);
 
 COMMIT TRANSACTION;
